8ddb1d8a3446094dbf387e70c59496ba66ea900d
Adam Palazzo Ginzberg <ginzberg@corn17.stanford.edu>
2017-02-26 14:29:20 -0800

    creating the feature/label mapping correctly now

--- utils/code_comp_utils.py
+++ utils/code_comp_utils.py
@@ -1,12 +1,10 @@
 import json
 import os
 import time
-from tree_utils import ast_to_lcrs
-from token_mapping import tree_traversal
+from tree_utils import ast_to_lcrs, tree_traversal
 
 """
-Largely inspired by code from cs224n PSET 2 for the 
-dependency parser
+Largely inspired by code from cs224n PSETs 2 and 3 
 """
 
 I_PREFIX = '<i>:'
@@ -27,9 +25,12 @@ class Config(object):
     reduced_train = 500
     reduced_dev = 100
     reduced_test = 100
+    segment_size = 50
     train_file = 'programs_training.json'
     dev_file = 'programs_dev.json'
     test_file = 'programs_test.json'
+
+config = Config()
 
 class CodeCompleter(object):
     def __init__(self, dataset):
@@ -111,9 +112,27 @@ class CodeCompleter(object):
         # called from create_instances
         pass
 
-def read_data(infile, reduced=False, num_examples=None):
+def process_token_list(token_list, non_terminal):
     '''
-    This reads in the asts from the file and converts them to binary trees.
+    Takes the token_list and separates it into features and label for each
+    segment.
+    '''
+    token_list = token_list[:-1] # Drop last token which is always program
+    segments = []
+    for i in xrange(0, len(token_list), config.segment_size):
+        segment = token_list[i:i + config.segment_size]
+        features = []
+        for tup in segment[:-1]:
+            features.extend(list(tup))
+        idx = 0 if non_terminal else 1
+        label = [token_list[-1][idx]]
+        segments.append([features, label])
+    return segments
+
+def read_json(infile, reduced=False, num_examples=None, non_terminal=True):
+    '''
+    This reads in the asts from the file and converts them to binary trees
+    and then token lists.
     '''
     examples = []
  
@@ -121,8 +140,9 @@ def read_data(infile, reduced=False, num_examples=None):
         for line in f:
             data = json.loads(line)
             binarized = ast_to_lcrs(data)
-            token_list, terminal_map, non_terminal_map = tree_traversal(binarized)
-            examples.append(token_list)
+            token_list = tree_traversal(binarized)
+            segments = process_token_list(token_list, non_terminal)
+            examples.extend(segments)
             if reduced:
                 num_examples -= 1
                 if num_examples == 0:
@@ -131,16 +151,15 @@ def read_data(infile, reduced=False, num_examples=None):
     return examples
 
 def load_and_preprocess_data(reduced=True):
-    config = Config()
     
     print "Loading data...",
     start = time.time()
 
-    train_set = read_data(os.path.join(config.data_path, config.train_file),
+    train_set = read_json(os.path.join(config.data_path, config.train_file),
                           reduced, config.reduced_train)
-    dev_set = read_data(os.path.join(config.data_path, config.dev_file),
+    dev_set = read_json(os.path.join(config.data_path, config.dev_file),
                         reduced, config.reduced_dev)
-    test_set = read_data(os.path.join(config.data_path, config.test_file),
+    test_set = read_json(os.path.join(config.data_path, config.test_file),
                          reduced, config.reduced_test)
     print "took {:.2f} seconds".format(time.time() - start)
     
--- utils/tree_utils.py
+++ utils/tree_utils.py
@@ -1,59 +1,90 @@
 import json
 
+EMPTY = "<EMPTY>"
+NON_ASCII = "<NON_ASCII>"
+
 class LCRSNode():
 
-	def __init__(self, node_id, data):
-		self.id = node_id
-		self.data = data
-		self.left_child = None
-		self.right_child = None
+    def __init__(self, node_id, data):
+        self.id = node_id
+        self.data = data
+        self.left_child = None
+        self.right_child = None
 
-	def add_left(self, node):
-		self.left_child = node
-		return node
+    def add_left(self, node):
+        self.left_child = node
+        return node
 
-	def add_right(self, node):
-		self.right_child = node
-		return node
+    def add_right(self, node):
+        self.right_child = node
+        return node
 
-	def __repr__(self, level=0):
-		ret = "\t"*level+str(self.id)+"\n"
-		if self.left_child is not None:
-			ret += self.left_child.__repr__(level+1)
-		if self.right_child is not None:
-			ret += self.right_child.__repr__(level+1)
-		return ret
-
-class LCRSTree():
-
-	def __init__(self, root):
-		self.root = root
+    def __repr__(self, level=0):
+        ret = "\t"*level+str(self.id)+"\n"
+        if self.left_child is not None:
+            ret += self.left_child.__repr__(level+1)
+        if self.right_child is not None:
+            ret += self.right_child.__repr__(level+1)
+        return ret
 
 def constructTree(ast):
-	children_dic = dict()
-	nodes = dict()
-	for data in ast:
-		if data == 0:
-			continue
-		node_id = data.pop('id', None)
-		children = data.pop('children', [])
-		tree_node = LCRSNode(node_id, data)
-		children_dic[node_id] = children
-		nodes[node_id] = tree_node
+    children_dic = dict()
+    nodes = dict()
+    for data in ast:
+        if data == 0:
+            continue
+        node_id = data.pop('id', None)
+        children = data.pop('children', [])
+        tree_node = LCRSNode(node_id, data)
+        children_dic[node_id] = children
+        nodes[node_id] = tree_node
 
-	tree = LCRSTree(nodes[0])
-	for node_id, children in children_dic.iteritems():
-		if len(children) > 0:
-			curr = nodes[node_id].add_left(nodes[children[0]])
-			if len(children) > 1:
-				for child in children[1:]:
-					curr = curr.add_right(nodes[child])
+    tree = nodes[0]
+    for node_id, children in children_dic.iteritems():
+        if len(children) > 0:
+            curr = nodes[node_id].add_left(nodes[children[0]])
+            if len(children) > 1:
+                for child in children[1:]:
+                    curr = curr.add_right(nodes[child])
 
-	return tree
+    return tree
 
 def ast_to_lcrs(ast):
-	tree = constructTree(ast)
-	return tree
+    tree = constructTree(ast)
+    return tree
 
-if __name__ == "__main__":
-	main()
+def tree_traversal(root):
+    token_list = []
+    inorder(None, root, token_list)
+    return token_list
+
+def convert_to_token(parent, node):
+    T_i = EMPTY
+    N_i_termChild = 0
+    if node.data.get("value", None) is not None:
+        try:
+            T_i = str(node.data["value"])
+        except:
+            T_i = NON_ASCII
+            N_i_termChild = 1
+
+    N_i_rightSib = 0
+    if parent is not None:
+        if parent.right_child is not None:
+            N_i_rightSib = 1
+
+    N_i = (node.data["type"], N_i_termChild, N_i_rightSib)
+    
+    return (N_i, T_i)
+
+def inorder(parent, node, arr):
+    if not node:
+        return None
+
+    inorder(node, node.left_child, arr) #left child
+    
+    token = convert_to_token(parent, node)
+    arr.append(token)
+    
+    inorder(node, node.right_child, arr) #right child
+
